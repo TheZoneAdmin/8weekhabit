b366b505a59628e6e259121c7fd1264890543c6d
TheZoneAdmin <info@zoneneston.co.uk>
2024-12-10 18:58:32 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-"use client"
+'use client';
 
-import HabitProgram from '@/components/habit-program'
+import dynamic from 'next/dynamic'
+
+const HabitProgram = dynamic(() => import('@/components/habit-program'), {
+  ssr: false // This disables server-side rendering for this component
+})
 
 export default function Home() {
   return (
